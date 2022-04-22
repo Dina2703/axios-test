89babe853bc516d0aa26e2bb14976d235b8ad845
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-22 11:55:03 -0400

    GET request via axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
 function App() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((res) => {
+      setPost(res.data);
+    });
+  }, []);
+
+  if (!post) {
+    return null;
+  }
   return (
     <div className="App">
-      <h2>Axios examples</h2>
+      <h2>Axios examples:</h2>
+      <div>Post title:</div>
+      <h5> {post.title}</h5>
+      <div>Post body:</div>
+      <p> {post.body}</p>
     </div>
   );
 }
